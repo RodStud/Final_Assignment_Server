4d1b3176e646f24db4dd015ec460bacb6f3ae379
RodStud <rodion.novakovskiy@stu.shenkar.ac.il>
2024-07-28 17:45:10 +0300

    Created "POST /login" route.

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
+const {user_controller} = require('./controllers/user_controller.js');
+
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.post("/login", async function (request, response) {
+    let result = await user_controller.authorization(request.body);
+    if (result == 401) {return response.status(401).send("Incorrect login or password");}
+    return response.json(result);
+});
 
 app.listen(port, host, () => {
     console.log(`Server listens http://${host}:${port}`);
