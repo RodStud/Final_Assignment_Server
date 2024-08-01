88a525cdb347d4cfc4f7480d780c785052b63e80
RodStud <rodion.novakovskiy@stu.shenkar.ac.il>
2024-08-02 02:56:28 +0300

    CORS settings changed.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser(secret));
 app.use(session({
     secret: secret,
     cookie: {
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24
     }
 }));
