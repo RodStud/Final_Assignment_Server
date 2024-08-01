421b1ced671302ae5debecba26cb361d72324d0b
RodStud <rodion.novakovskiy@stu.shenkar.ac.il>
2024-08-02 02:50:23 +0300

    Changed "User Controller".

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,30 +2,33 @@ const {db_controller} = require("./db_controller");
 
 exports.user_controller = {
 
-    async authorization(data) {
+    async authorization(login, password) {
         try {
             let result = await db_controller.sql_execute(`
                 SELECT u.id AS user_id, r.role_name AS role
                 FROM tbl_118_user AS u
                          INNER JOIN tbl_118_role AS r ON u.role_id = r.id
-                WHERE u.login = "${data.login}"
-                  AND u.password = "${data.password}"
+                WHERE u.login = "${login}"
+                  AND u.password = "${password}"
             `);
-            return result;
+            if (result.length > 0) {
+                return result
+            }
+            return 401;
         } catch (error) {
             return 401;
         }
     },
 
-    async get_user(data) {
+    async get_user(patient_id) {
         try {
             let result = await db_controller.sql_execute(`
                 SELECT *
                 FROM tbl_118_user AS u
                          INNER JOIN tbl_118_role AS r ON u.role_id = r.id
-                WHERE u.id = ${data.user_id}
+                WHERE u.id = (SELECT user_id FROM tbl_118_patient WHERE id = ${patient_id})
             `);
-            return result;
+            return result[0];
         } catch (error) {
             return 400;
         }
@@ -39,7 +42,9 @@ exports.user_controller = {
                         "${data.last_name}", "${data.telephone}", "${data.email}",
                         ${data.role_id})
             `);
-            return 200;
+            result = await this.authorization(data.login, data.password);
+            return result[0].user_id;
+
         } catch (error) {
             return 400;
         }
@@ -72,18 +77,80 @@ exports.user_controller = {
         }
     },
 
-    async create_child(data) {
+    async create_child(data, doctor_id) {
+                if (data.child.login.length < 9) {
+                    return 400;
+                }
         try {
             let result = await this.create_user(data.child);
-            result = await this.create_patient(data.child);
-            if (data.parent_1 != null) {
+            data.child.user_id = result;
+            let temp = await this.create_patient(data.child);
+            result = await  db_controller.sql_execute(`SELECT id FROM tbl_118_patient WHERE user_id = ${result}`);
+            result = await this.create_therapy(doctor_id, result[0].id);
+            result = await this.connect_child_to_parent(data.child);
+
+            if (data.parent_1.login) {
                 result = await this.create_user(data.parent_1);
+                data.parent_1.user_id = result;
                 result = await this.connect_child_to_parent(data.child.user_id, data.parent_1.user_id);
             }
-            if (data.parent_2 != null) {
+            if (data.parent_2.login) {
                 result = await this.create_user(data.parent_2);
+                data.parent_2.user_id = result.id;
                 result = await this.connect_child_to_parent(data.child.user_id, data.parent_2.user_id);
             }
+            return 200;
+        } catch (error) {
+            return 400;
+        }
+    },
+
+    async get_hobbies() {
+        try {
+            let result = await db_controller.sql_execute(`SELECT *
+                                                          FROM tbl_118_hobby`);
+            return result;
+        } catch (error) {
+            return 400;
+        }
+    },
+
+    async get_hmo() {
+        try {
+            let result = await db_controller.sql_execute(`SELECT *
+                                                          FROM tbl_118_hmo`);
+            return result;
+        } catch (error) {
+            return 400;
+        }
+    },
+
+    async get_medical_condition() {
+        try {
+            let result = await db_controller.sql_execute(`SELECT *
+                                                          FROM tbl_118_medical_condition`);
+            return result;
+        } catch (error) {
+            return 400;
+        }
+    },
+
+    async get_child_list(parent_id) {
+        try {
+            let result = await db_controller.sql_execute(`SELECT child_id
+                                                          FROM tbl_118_parent
+                                                          WHERE parent_id = ${parent_id}`);
+            return result[0].child_id;
+        } catch (error) {
+            return 400;
+        }
+    },
+
+    async create_therapy(doctor_id, patient_id) {
+        try {
+            let result = await db_controller.sql_execute(`
+                INSERT INTO tbl_118_therapy (doctor_id, patient_id) VALUES (${doctor_id}, ${patient_id})
+            `);
             return 200;
         } catch (error) {
             return 400;
