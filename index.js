41218fd1ee8fbdcb202c5492b0359a61c2b51746
RodStud <rodion.novakovskiy@stu.shenkar.ac.il>
2024-08-02 03:27:22 +0300

    Cookie settings changed.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser(secret));
 app.use(session({
     secret: secret,
     cookie: {
-        secure: true,
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24
     }
 }));
